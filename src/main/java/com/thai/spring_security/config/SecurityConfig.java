3e38112db299a478fe6af58c957e967369e93e2a
ThaigoDev <thaigodeveloper@gmail.com>
2026-02-13 15:50:16 -0300

    jwt condig

---
+++ src/main/java/com/thai/spring_security/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.thai.spring_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+   @Bean
+    private SecurityFilterChain securityFilterChain (HttpSecurity http) {
+        http.authorizeHttpRequests(authorize-> authorize.anyRequest().authenticated())
+                .csrf(csrf -> csrf.disable()).oauth2ResourceServer(auth -> auth.jwt(Customizer.withDefaults())).sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+   }
+}
